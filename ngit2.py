d7e78d8df2030978a8a1e522bd5eef53fcae2016
codinggandhi <116358825+codinggandhi@users.noreply.github.com>
2022-10-22 10:21:00 +0530

    identations corrected

--- ngit2.py
+++ ngit2.py
@@ -12,10 +12,10 @@ print ("The original string is : " + test_str)
 # Maximum frequency character in String
 all_freq = {}
 for i in test_str:
-if i in all_freq:
-all_freq[i] += 1
-else:
-all_freq[i] = 1
+  if i in all_freq:
+    all_freq[i] += 1
+  else:
+    all_freq[i] = 1
 res = max(all_freq, key = all_freq.get)
 
 # printing result
